aac0576aa04ae4f241849ed4ffbea8ceffa579f3
Lucas Jagg <lukejagg@gmail.com>
2021-04-19 22:07:54 -0500

    Initial commit

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InfiniteMaze
+{
+	public class Generator : MonoBehaviour
+	{
+		public Transform mazePrefab;
+
+		public int cellWidth, cellHeight;
+
+		public Dictionary<Position, Maze> Mazes = new Dictionary<Position, Maze>();
+
+		void CreateMaze(Position pos)
+		{
+			if (Mazes.ContainsKey(pos)) return;
+
+			var rnd = new System.Random(pos.X * 100320 + pos.Y);
+
+			var obj = new GameObject(pos.ToString()).transform;
+			obj.localPosition = new Vector3Int(pos.X * (cellWidth * 2), 0, pos.Y * (cellHeight * 2));
+
+			var maze = new Maze(cellWidth, cellHeight);
+			maze.Generate(rnd);
+
+
+			int badX = rnd.Next(0, cellWidth) * 2 + 1;
+			int badY = rnd.Next(0, cellHeight) * 2 + 1;
+
+			for (int i = 0; i < maze.Width - 1; i++)
+			{
+				for (int j = 0; j < maze.Height - 1; j++)
+				{
+					if (i == 0 && j == badY || i == badX && j == 0)
+						continue;
+
+					if (!maze[i, j])
+					{
+						var t = Instantiate(mazePrefab, obj);
+						t.localPosition = new Vector3(i, 0, j);
+					}
+				}
+			}
+
+			Mazes.Add(pos, maze);
+		}
+
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			NextPosition.Add(new Position(0, 0));
+		}
+
+		void Add(Position p)
+		{
+			if (!Generated.Contains(p))
+				NextPosition.Add(p);
+		}
+
+		HashSet<Position> Generated = new HashSet<Position>();
+		List<Position> NextPosition = new List<Position>();
+		void Update()
+		{
+			var p = NextPosition[0];
+			NextPosition.RemoveAt(0);
+
+			if (!Generated.Contains(p))
+			{
+				Generated.Add(p);
+				CreateMaze(p);
+
+				Add(p + new Position(0, 1));
+				Add(p + new Position(0, -1));
+				Add(p + new Position(1, 0));
+				Add(p + new Position(-1, 0));
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using Random = System.Random;
+
+namespace InfiniteMaze
+{
+	public struct Position
+	{
+		public int X { get; }
+		public int Y { get; }
+
+		public Position(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public static Position operator +(Position a, Position b) => new Position(a.X + b.X, a.Y + b.Y);
+
+		public override string ToString()
+		{
+			return $"{X}, {Y}";
+		}
+	}
+
+	public class Maze
+	{
+		public int CellWidth { get; }
+		public int CellHeight { get; }
+
+		public int Width { get; }
+		public int Height { get; }
+
+		public bool this[Position pos] => InMaze(pos) ? cells[pos.X, pos.Y] : false;
+		public bool this[int x, int y] => this[new Position(x, y)];
+
+		private bool[,] cells;
+
+		public Maze(int cellWidth, int cellHeight)
+		{
+			CellWidth = cellWidth;
+			CellHeight = cellHeight;
+
+			Width = cellWidth * 2 + 1;
+			Height = cellHeight * 2 + 1;
+
+			cells = new bool[Width, Height];
+		}
+
+		bool InMaze(Position pos)
+		{
+			return pos.X >= 0 && pos.X < Width &&
+			       pos.Y >= 0 && pos.Y < Height;
+		}
+
+		bool CanMoveDirection(Position pos, Position dir)
+		{
+			var next = pos;
+			for (int i = 0; i < 2; i++)
+			{
+				next += dir;
+				if (!InMaze(next) || cells[next.X, next.Y])
+					return false;
+			}
+
+			return true;
+		}
+
+		// Regenerates this maze using recursive backtracking
+		public void Generate(System.Random rnd)
+		{
+			var up = new Position(0, 1);
+			var right = new Position(1, 0);
+			var down = new Position(0, -1);
+			var left = new Position(-1, 0);
+
+			// Convert to maze coordinates
+			var startX = rnd.Next(0, CellWidth) * 2 + 1;
+			var startY = rnd.Next(0, CellHeight) * 2 + 1;
+
+			// Reset all cells in the maze
+			for (int x = 0; x < Width; x++)
+			for (int y = 0; y < Height; y++)
+				cells[x, y] = false;
+
+			// Path, used for backtracking
+			var path = new List<Position>(Width);
+			cells[startX, startY] = true;
+			path.Add(new Position(startX, startY));
+
+			var possibleDirections = new List<Position>();
+
+			// Continue until there are no more open cells
+			while (path.Count > 0)
+			{
+				var pos = path[path.Count - 1];
+
+				// Get possible directions
+				possibleDirections.Clear();
+				if (CanMoveDirection(pos, up)) possibleDirections.Add(up);
+				if (CanMoveDirection(pos, right)) possibleDirections.Add(right);
+				if (CanMoveDirection(pos, down)) possibleDirections.Add(down);
+				if (CanMoveDirection(pos, left)) possibleDirections.Add(left);
+
+				// Move in a random direction
+				if (possibleDirections.Count > 0)
+				{
+					var dir = possibleDirections[rnd.Next(0, possibleDirections.Count)];
+
+					// Carve Path
+					for (int i = 0; i < 2; i++)
+					{
+						pos += dir;
+						cells[pos.X, pos.Y] = true;
+					}
+
+					path.Add(pos);
+				}
+				else
+				{
+					// No other places to move, backtrack
+					path.RemoveAt(path.Count - 1);
+				}
+			}
+		}
+
+		// Text representation of the cells
+		public override string ToString()
+		{
+			var str = "";
+			for (int x = 0; x < Width; x++)
+			{
+				for (int y = 0; y < Height; y++)
+					str += cells[x, y] ? 'O' : '_';
+				str += '\n';
+			}
+
+			return str.TrimEnd('\n');
+		}
+	}
+}
